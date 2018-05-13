e1ec6fa0873fffce6b782cee1ed9678e18a0a3a5
Tyler Maxwell <tmax818@mac.com>
2018-05-13 12:25:31 -0700

    Add bootstrap gem and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :books
   resources :users
+
+  root 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
