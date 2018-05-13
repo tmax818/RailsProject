2fbb1dea2fa21d426d1b542310717f3fcf18286a
Tyler Maxwell <tmax818@mac.com>
2018-05-13 08:52:40 -0700

    Add uniqueness validation for email with tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,5 +48,10 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
