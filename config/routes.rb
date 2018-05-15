65379e690719809fcee92dba1906e5f9438056f2
Tyler Maxwell <tmax818@mac.com>
2018-05-15 09:29:42 -0700

    Add routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
   root 'static#home'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  get 'about',       to: 'static#about'
+  get 'help',       to: 'static#help'
+  get 'signup',     to: 'users#new'
+  post 'signup',    to: 'users#create'
 
 
   resources :genres
   resources :reviews
   resources :books
-  resources :users 
+  resources :users
 
 
 end
