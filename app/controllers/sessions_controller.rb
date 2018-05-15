aedeffb218c23919323ba7d425f51eb04c7965a7
Tyler Maxwell <tmax818@mac.com>
2018-05-15 12:23:35 -0700

    Add session helpers and flash messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = "Welcome Back #{user.name.capitalize}"
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid email/password combination."
+      render :new
+    end
   end
 
   def destroy
