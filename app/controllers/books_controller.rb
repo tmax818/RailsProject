e277c357cfed4613965aa409cbb160c9f38e1f3c
Tyler Maxwell <tmax818@mac.com>
2018-05-16 18:14:58 -0700

    Add links partial and activated stubbed links.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,7 @@
 class BooksController < ApplicationController
 
-  def index
-
-  end
-
-  def show
-
-  end
-
   def new
 
   end
 
-  def edit
-
-  end
-
-  def create
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
 end
