d8d1105632fa73511cc5592ac6a186d76607ac24
Tyler Maxwell <tmax818@mac.com>
2018-05-17 09:53:19 -0700

    Add nested new form review for user.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,19 @@ class ReviewsController < ApplicationController
   def show
     @review = Review.find(params[:id])
   end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.create(review_params)
+    @review.user_id = current_user.id 
+    debugger
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:content, :book_id)
+    end
 end
