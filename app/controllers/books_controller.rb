e0a299699b82e9c7a0dd26774e6a103ad0787538
Tyler Maxwell <tmax818@mac.com>
2018-05-16 19:43:30 -0700

    Add nested index page with books index nested in users show page.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,21 @@
 class BooksController < ApplicationController
 
+
+  def index
+    if params[:user_id]
+      @books = User.find(params[:user_id]).books
+    end
+  end
+
+
   def new
 
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
 
 end
